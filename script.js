e0981b251b3e8ff319c311034be452724dae51ed
2471186676 <alex145561625@gmail.com>
2022-03-24 15:46:55 +0800

    add left nav bar & new font

---
+++ script.js
@@ -0,0 +1,29 @@
+let pageNav = document.getElementsByClassName("pageNav")[0];
+console.log(pageNav);
+
+let option1 = ["Home", "Profile", "Message", "History", "Tasks", "community"];
+let option2 = ["Setting", "Support", "Privacy"];
+
+let createOption = (option) =>{
+    for(let i = 0; i < option.length; i++){
+        let child = document.createElement("button");
+        child.className = "option"
+
+        let img = document.createElement("img");
+        img.src = "./img/" +option[i] + "jpeg";
+        img.alt = "img";
+        child.appendChild(img);
+
+        let text = document.createElement("p");
+        text.innerText = option[i];
+        child.appendChild(text);
+
+        pageNav.appendChild(child);
+    }
+}
+
+createOption(option1);
+let separater = document.createElement("button");
+separater.className="separater"
+pageNav.appendChild(separater);
+createOption(option2);
